b8cb492b33b8da040376013872fd2e6b883d5f46
mozahidhusain007 <114985088+mozahidhusain007@users.noreply.github.com>
2022-12-30 17:29:51 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,1 +1,17 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
 
+  name = "sparking"
+
+  ami                    = var.ec2_instance_id
+  instance_type          = var.ec2_instance_type
+  key_name               = var.ec2_key_name
+  vpc_security_group_ids = var.ec2_security_group_ids
+  subnet_id              = var.ec2_subnet_ids
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
